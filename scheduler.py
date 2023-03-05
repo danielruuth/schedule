ac047d14739938af69dc6ed43b85a41923e02a01
danielruuth <daniel.ruuth@gmail.com>
2023-03-05 19:05:32 +0100

    Update scheduler.py

--- scheduler.py
+++ scheduler.py
@@ -21,16 +21,10 @@ import json
 from ortools.sat.python import cp_model
 from google.protobuf import text_format
 
-_OUTPUT_PROTO = flags.DEFINE_string(
-    'output_proto', '', 'Output file to write the cp_model proto to.')
-_PARAMS = flags.DEFINE_string('params', 'max_time_in_seconds:10.0',
-                              'Sat solver parameters.')
-
-
 class Scheduler:
-    def __init__(self, _PARAMS, _OUTPUT_PROTO):
-        self._params = _PARAMS.value
-        self._proto = _OUTPUT_PROTO.value
+    def __init__(self, params):
+        self._params = params
+        
         
     def negated_bounded_span(works, start, length):
         sequence = []
@@ -119,57 +113,26 @@ class Scheduler:
 
         return cost_variables, cost_coefficients
 
-    def solve():
+    def solve(self):
         """Solves the shift scheduling problem."""
         # Data
-        num_employees = 8
-        num_weeks = 3
-        shifts = ['O', 'M', 'A', 'N']
+        num_employees = len(self.params.resources) #8
+        num_weeks = self.params.weeks #3
+        shifts = self.params.shifts #['O', 'A', 'C'] # Off, day, night
 
         # Fixed assignment: (employee, shift, day).
         # This fixes the first 2 days of the schedule.
-        fixed_assignments = [
-            (0, 0, 0),
-            (1, 0, 0),
-            (2, 1, 0),
-            (3, 1, 0),
-            (4, 2, 0),
-            (5, 2, 0),
-            (6, 2, 3),
-            (7, 3, 0),
-            (0, 1, 1),
-            (1, 1, 1),
-            (2, 2, 1),
-            (3, 2, 1),
-            (4, 2, 1),
-            (5, 0, 1),
-            (6, 0, 1),
-            (7, 3, 1),
-        ]
+        # This should come from params
+        fixed_assignments = self.params.fixed_assignments
 
         # Request: (employee, shift, day, weight)
         # A negative weight indicates that the employee desire this assignment.
-        requests = [
-            # Employee 3 does not want to work on the first Saturday (negative weight
-            # for the Off shift).
-            (3, 0, 5, -2),
-            # Employee 5 wants a night shift on the second Thursday (negative weight).
-            (5, 3, 10, -2),
-            # Employee 2 does not want a night shift on the first Friday (positive
-            # weight).
-            (2, 3, 4, 4)
-        ]
+        requests = self.params.requests
 
         # Shift constraints on continuous sequence :
         #     (shift, hard_min, soft_min, min_penalty,
         #             soft_max, hard_max, max_penalty)
-        shift_constraints = [
-            # One or two consecutive days of rest, this is a hard constraint.
-            (0, 1, 1, 0, 2, 2, 0),
-            # between 2 and 3 consecutive days of night shifts, 1 and 4 are
-            # possible but penalized.
-            (3, 1, 2, 20, 3, 4, 5),
-        ]
+        shift_constraints = self.params.shift_constraints
 
         # Weekly sum constraints on shifts days:
         #     (shift, hard_min, soft_min, min_penalty,
@@ -177,30 +140,20 @@ class Scheduler:
         weekly_sum_constraints = [
             # Constraints on rests per week.
             (0, 1, 2, 7, 2, 3, 4),
-            # At least 1 night shift per week (penalized). At most 4 (hard).
-            (3, 0, 1, 3, 4, 4, 0),
+            # At least 1 evening shift per week (penalized). At most 4 (hard).
+            (2, 0, 1, 3, 4, 4, 0),
         ]
 
         # Penalized transitions:
         #     (previous_shift, next_shift, penalty (0 means forbidden))
         penalized_transitions = [
-            # Afternoon to night has a penalty of 4.
-            (2, 3, 4),
-            # Night to morning is forbidden.
-            (3, 1, 0),
+            # Night to morning has a penalty of 4.
+            (2, 1, 4),
         ]
 
-        # daily demands for work shifts (morning, afternon, night) for each day
+        # daily demands for work shifts (day, night) for each day
         # of the week starting on Monday.
-        weekly_cover_demands = [
-            (2, 3, 1),  # Monday
-            (2, 3, 1),  # Tuesday
-            (2, 2, 2),  # Wednesday
-            (2, 3, 1),  # Thursday
-            (2, 2, 2),  # Friday
-            (1, 2, 3),  # Saturday
-            (1, 3, 1),  # Sunday
-        ]
+        weekly_cover_demands = self.params.cover_demans
 
         # Penalty for exceeding the cover constraint per shift type.
         excess_cover_penalties = (2, 2, 5)
@@ -306,15 +259,8 @@ class Scheduler:
             sum(obj_int_vars[i] * obj_int_coeffs[i]
                 for i in range(len(obj_int_vars))))
 
-        if self._proto:
-            print('Writing proto to %s' % self._proto)
-            with open(self._proto, 'w') as text_file:
-                text_file.write(str(model))
-
         # Solve the model.
         solver = cp_model.CpSolver()
-        if self._params:
-            text_format.Parse(self._params, solver.parameters)
         solution_printer = cp_model.ObjectiveSolutionPrinter()
         status = solver.Solve(model, solution_printer)
 
@@ -366,7 +312,57 @@ class Scheduler:
 
 
 def main(_):
-    const s = Scheduler(_PARAMS, _OUTPUT_PROTO)
+    params = {
+        resources: ['Jim', 'Pam', 'Dwight', 'Stan', 'Creed', 'Andy', 'Michael', 'Phyllis'],
+        shifts: ['O', 'A', 'C'], # Off, day, night
+        weeks: 3,
+        fixed_assignments: [
+            (0, 0, 0),
+            (1, 0, 0),
+            (2, 1, 0),
+            (3, 1, 0),
+            (4, 2, 0),
+            (5, 2, 0),
+            (6, 2, 3),
+            (7, 3, 0),
+            (0, 1, 1),
+            (1, 1, 1),
+            (2, 2, 1),
+            (3, 2, 1),
+            (4, 2, 1),
+            (5, 0, 1),
+            (6, 0, 1),
+            (7, 3, 1),
+        ],
+        requests: [
+            # Employee 3 does not want to work on the first Saturday (negative weight
+            # for the Off shift).
+            (3, 0, 5, -2),
+            # Employee 5 wants a night shift on the second Thursday (negative weight).
+            (5, 3, 10, -2),
+            # Employee 2 does not want a night shift on the first Friday (positive
+            # weight).
+            (2, 3, 4, 4)
+        ],
+        shift_constraints:  [
+            # One or two consecutive days of rest, this is a hard constraint.
+            (0, 1, 1, 0, 2, 2, 0)
+        ],
+        
+        cover_demands: [
+            (3, 3),  # Monday
+            (3, 3),  # Tuesday
+            (3, 3),  # Wednesday
+            (3, 3),  # Thursday
+            (3, 3),  # Friday
+            (2, 2),  # Saturday
+            (2, 2),  # Sunday
+        ]
+        
+    }
+    
+        
+    s = Scheduler(params)
     s.solve()
 
 
